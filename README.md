ea0fa7ad3e6c7b7d20748a40fa246b7bc2512df4
QUSIR <1326714828@qq.com>
2018-02-01 16:07:43 +0800

    将工程改为android.mk方式编译jni的so文件

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # android gpio口控制
+
+在程序运行之前用ndk-build编译一遍生成so文件。
+
+![view](./Android_GPIO_DEMO.png)
---
+++ app/src/main/jni/Application.mk
@@ -0,0 +1,4 @@
+APP_STL:= stlport_static
+APP_PLATFORM := android-22
+APP_ABI := armeabi
+#APP_ABI := armeabi-v7a
